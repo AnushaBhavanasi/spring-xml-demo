6644137e7df8ff3064cb635c152ee2d1bca29c29
anusha <vanushabhavanasi@gmail.com>
2019-05-24 14:51:35 +0530

    "task3 semi completed"

--- src/main/java/com/stackroute/MainClass.java
+++ src/main/java/com/stackroute/MainClass.java
@@ -14,31 +14,14 @@ import org.springframework.core.io.ClassPathResource;
 
 public class MainClass
 {
-    @Deprecated
+
     public static void main( String[] args )
     {
-        ClassPathResource resource= new ClassPathResource("beans.xml");
-        BeanFactory beanFactory = new XmlBeanFactory(resource);
-        Movie movie= (Movie) beanFactory.getBean("movies");
-        System.out.println("Actor information using bean factory:\n "+movie);
-        ApplicationContext applicationContext=new ClassPathXmlApplicationContext("beans.xml");
-        Movie mv=(Movie) applicationContext.getBean("movies");
-        System.out.println("Actor information using application context:\n "+mv);
-        BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
-        BeanDefinitionReader  beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-        Movie mo=(Movie)((DefaultListableBeanFactory)beanDefinitionRegistry).getBean("movies1");
-        System.out.println("Actor information using application context:\n "+mo);
-        //testing the scope
         ApplicationContext application=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie2=(Movie) application.getBean("movies");
-        Movie movie3=(Movie) applicationContext.getBean("movies");
-        System.out.println(movie2==movie3);
+        Movie movie=(Movie)application.getBean("movies");
+        System.out.println("Changing the Actor constructor to autowire \n"+movie);
+        Movie movie1=(Movie)application.getBean("movie");
+        System.out.println("Changing the Actor constructor to autowire to byType \n"+movie1);
 
-        Movie movie1=(Movie) application.getBean("MovieA");
-        System.out.println("Changing the id of beans"+movie1);
-        //changing the id to name in beans.xml
-        Movie movie4=(Movie) application.getBean("MovieB");
-        System.out.println("Changing the id of beans"+movie4);
     }
 }
